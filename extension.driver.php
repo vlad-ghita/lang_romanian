6e5ad8c23195d917ca88eb303bfdc5245a2533ff
Vlad <vlad_micutul@yahoo.com>
2011-02-09 14:38:20 +0200

    Updated about info.

--- extension.driver.php
+++ extension.driver.php
@@ -17,22 +17,6 @@
 					'email' => 'vlad_micutul@yahoo.com'
 				),
 				'description' => 'Romanian translation for the Symphony backend',
-				'compatibility' => array(
-					'2.0.0' => unsure,
-					'2.0.1' => unsure,
-					'2.0.2' => unsure,
-					'2.0.3' => unsure,
-					'2.0.4' => unsure,
-					'2.0.5' => unsure,
-					'2.0.6' => unsure,
-					'2.0.7' => unsure,
-					'2.0.8' => unsure,
-					'2.0.9' => unsure,
-					'2.1.0' => unsure,
-					'2.1.1' => true,
-					'2.1.2' => true,
-					'2.2' => true,
-				)
 			);
 		}
 
